1e66a3e20115e7b39b167bcf628d724e80b1d3db
aranaris <vinceye.cm@gmail.com>
2025-07-13 10:46:09 -0700

    update reader to parse from stream of data

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,14 +1,34 @@
 package request
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"regexp"
 	"strings"
 )
 
+type Status int
+const (
+	Initialized Status = iota
+	Done
+)
+
+func (s Status) String() string {
+	switch s {
+	case 0:
+		return "Initialized"
+	case 1:
+		return "Done"
+	}
+	return fmt.Sprintf("Status(%q)", int(s))
+}
+
+var BufferSize = 8
+
 type Request struct {
 	RequestLine RequestLine
+	Status 			Status
 }
 
 type RequestLine struct {
@@ -18,21 +38,48 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return nil, err
-	}
-
-	rl, _, err := parseRequestLine(data)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return nil, err
-	}
 
 	var r Request
-	r.RequestLine = rl
-	return &r, nil
+	buffer := make([]byte, BufferSize)
+	r.Status = 0
+	readToIndex := 0
+	parsed := 0
+
+	for {
+		if r.Status == 1 {
+			return &r, nil
+		}
+
+		if readToIndex >= len(buffer) {
+			newSize := len(buffer) + BufferSize
+			temp := buffer
+			buffer = make([]byte, newSize)
+			copy(buffer, temp)
+		}
+
+		b, err := reader.Read(buffer[readToIndex:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				r.Status = 1
+			}
+			return nil, err
+		}
+		readToIndex += b
+		
+		n, err := r.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		if n > 0 {
+			parsed += n
+			newSlice := make([]byte, BufferSize)
+			copy(newSlice, buffer[parsed:])
+			buffer = newSlice
+			readToIndex -= parsed
+		}
+
+	}
 }
 
 func parseRequestLine(data []byte) (RequestLine, int, error) {
@@ -66,5 +113,24 @@ func parseRequestLine(data []byte) (RequestLine, int, error) {
 
 	rq.Method = rs[0]
 	rq.RequestTarget = rs[1]
-	return rq, len(data), nil
+	return rq, len([]byte(segments[0])), nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	if r.Status != 0 {
+		return 0, fmt.Errorf("unable to parse request, not in initialized state")
+	}
+
+	rq, b, err := parseRequestLine(data)
+	if err != nil {
+		return 0, err
+	}
+
+	if b == 0 {
+		return 0, nil
+	}
+
+	r.RequestLine = rq
+	r.Status = 1
+	return b, nil
 }
