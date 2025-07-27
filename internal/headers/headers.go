30c09e4e20999f020d031e74c4f3a19c4adbcd4b
aranaris <vinceye.cm@gmail.com>
2025-07-26 17:17:25 -0700

    implement headers parse func

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,11 +1,50 @@
 package headers
 
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
 type Headers map[string]string
 
 func NewHeaders() (headers Headers) {
-	return nil
+	return make(map[string]string)
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	return 0, false, nil
+	d := false
+
+	clrf_regex := regexp.MustCompile(`\r\n`)
+	str := string(data)
+
+	loc := clrf_regex.FindStringIndex(str)
+	//No CLRF found indicates we need to continue parsing data
+	if loc == nil {
+		return 0, false, nil
+	}
+	//CLRF at the start of data, indicates end of headers
+	if loc[0] == 0 {
+		return 0, true, nil
+	}
+
+
+	trailing_r := regexp.MustCompile(`\s$`)
+	leading_r := regexp.MustCompile(`^\s`)
+
+	fn, fv, found := strings.Cut(str[0:loc[0]], ":")
+	if !found {
+		return 0, false, errors.New("missing semicolon, invalid header format")
+	}
+	if trailing_r.MatchString(fn) {
+		return 0, false, errors.New("invalid fieldname format in header")
+	}
+
+	fn = leading_r.ReplaceAllString(fn, "")
+	fv = leading_r.ReplaceAllString(fv, "")
+	fv = trailing_r.ReplaceAllString(fv, "")
+
+	h[fn] = fv
+
+	return len([]byte(str[0:loc[1]])), d, nil
 }
